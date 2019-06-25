6e63b59aaa3246b6e0b76aabb5c3db8ba39da2ae
lee <1575005415@163.com>
2019-06-25 15:49:02 +0800

    [add] junit使用

--- BasePointsMore/src/main/java/com/foroffer/App.java
+++ BasePointsMore/src/main/java/com/foroffer/App.java
@@ -2,12 +2,29 @@ package com.foroffer;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * 测试加法
+     * @param a 左参
+     * @param b 右参
+     * @return 返回值
+     */
+    public int add(int a ,int b){
+        return a+b;
+    }
+
+    /**
+     * 除法
+     * @param a 除数
+     * @param b 被除数
+     * @return 结果
+     */
+    public int div(int a,int b){
+        return  a/b;
     }
 }
--- BasePointsMore/src/test/java/com/foroffer/AppTest.java
+++ BasePointsMore/src/test/java/com/foroffer/AppTest.java
@@ -2,6 +2,9 @@ package com.foroffer;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,6 +13,13 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
+     * 用于测试房
+     */
+    @Before
+    public void init(){
+        System.out.println("测试方法初始化方法");
+    }
+    /**
      * Rigorous Test :-)
      */
     @Test
@@ -17,4 +27,29 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    /**
+     * 测试方法
+     */
+    @Test
+    public void testAdd(){
+        App app=new App();
+        int result=app.add(1,2);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDiv(){
+       App app=new App();
+       int tt=app.div(1,1);
+
+        Assert.assertEquals(tt,2);
+    }
+
+    @After
+    public void colse(){
+        System.out.println("关闭测试,,,,");
+    }
+
+
 }
